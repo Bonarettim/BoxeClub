113000c44a23bf89ad67b43536d15b08c3605927
Matheus <matheus@loop4.com.br>
2026-05-28 15:14:44 -0300

    refactor: adicionando a const de campoObrigatorio para deixar reutilizavel

--- components/Contact/validationSchema.ts
+++ components/Contact/validationSchema.ts
@@ -1,15 +1,17 @@
 import * as Yup from "yup";
 
+const campoObrigatorio = "Campo obrigatório";
+
 export const validationSchema = Yup.object({
   name: Yup.string()
     .min(3, "Minimo 3 caracteres")
-    .required("Campo obrigatório"),
+    .required(campoObrigatorio),
 
   email: Yup.string()
     .email("Email Invalido")
-    .required("Campo obrigatório"),
+    .required(campoObrigatorio),
 
   description: Yup.string()
     .min(10, "Minimo 10 caracteres")
-    .required("Campo obrigatório"),
+    .required(campoObrigatorio),
 });
